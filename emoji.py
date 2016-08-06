d0efc80bbadb43a9939b8b732afcc8e802fb12de
HZHT <htay.htat@gmail.com>
2016-08-07 01:11:12 +1000

    modify emoji; popup dialogbox

--- emoji.py
+++ emoji.py
@@ -5,33 +5,31 @@ http://emojipedia.org/samsung/
 '''
 
 from PyQt4 import QtGui, QtCore
+from collections import OrderedDict
 import sys
 
-emoji_dictionary = {':emo1:': '/images/emoji/bigsmile.png',
-                    ':emo2:': '/images/emoji/wideeyedsmile.png',
-                    ':emo3:': '/images/emoji/overjoyed.png',
-                    ':emo4:': '/images/emoji/thinking.png',
-                    ':emo5:': '/images/emoji/nerdluck.png',
-                    ':emo6:': '/images/emoji/moarnerdluck.png',
-                    ':emo7:': '/images/emoji/stoopidface.png',}
+emoji_dictionary = {':emo1:': 'images/emoji/bigsmile.png',
+                    ':emo2:': 'images/emoji/wideeyedsmile.png',
+                    ':emo3:': 'images/emoji/overjoyed.png',
+                    ':emo4:': 'images/emoji/thinking.png',
+                    ':emo5:': 'images/emoji/nerdluck.png',
+                    ':emo6:': 'images/emoji/moarnerdluck.png',
+                    ':emo7:': 'images/emoji/stoopidface.png',
+                    }
 
-class SelectEmoji(QtGui.QWidget):
+class SelectEmoji():
     def __init__(self):
-        QtGui.QWidget.__init__(self)
+        self.pick_n_choose = QtGui.QTextBrowser()
         self.initUI()
         
-    def init(self):
-        self.pick_n_choose = QtGui.QTextEdit()
-        pass
-            
+    def initUI(self):
+        for i in emoji_dictionary:
+            slot = QtGui.QPixmap(emoji_dictionary[i])
+            resized = slot.scaled(24, 24, QtCore.Qt.KeepAspectRatio).toImage()
+            self.pick_n_choose.textCursor().insertImage(resized)
+        self.show()
+
+    def show(self):
+        self.pick_n_choose.show()
         
-
-if __name__ == '__main__':
-    w = QtGui.QApplication(sys.argv)
-    z = QtGui.QPixmap(r'c:\temp\stoopid2.jpg')
-    zz = z.scaled(40, 0, QtCore.Qt.KeepAspectRatio).toImage()
-    a = QtGui.QTextEdit()
-    a.textCursor().insertImage(zz)
-    a.show()
-
-# emoji
+xxx = QtGui.QApplication(sys.argv) # remove after testing
