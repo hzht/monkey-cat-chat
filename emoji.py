491123fb63aa0d72aff8c3d0a63ea66f6ba24ae2
HZHT <htay.htat@gmail.com>
2016-08-07 23:16:02 +1000

    add more emoji

--- emoji.py
+++ emoji.py
@@ -31,34 +31,21 @@ class SelectEmoji(QtGui.QTextBrowser):
         self.setFixedSize(232,135)
 
         bs = '''&nbsp;&nbsp;&nbsp;'''
-        
         self.setHtml(
-            '<a href="::emo1:"><img src=' + ed[':emo1:'] + '/></a>'
-            +bs+
-            '<a href="::emo2:"><img src=' + ed[':emo2:'] + '/></a>'
-            +bs+
-            '<a href="::emo3:"><img src=' + ed[':emo3:'] + '/></a>'
-            +bs+
-            '<a href="::emo4:"><img src=' + ed[':emo4:'] + '/></a>'
-            +bs+
+            '<a href="::emo1:"><img src=' + ed[':emo1:'] + '/></a>'+bs+
+            '<a href="::emo2:"><img src=' + ed[':emo2:'] + '/></a>'+bs+
+            '<a href="::emo3:"><img src=' + ed[':emo3:'] + '/></a>'+bs+
+            '<a href="::emo4:"><img src=' + ed[':emo4:'] + '/></a>'+bs+
             '<a href="::emo5:"><img src=' + ed[':emo5:'] + '/></a>'
-            '<a href="::emo6:"><img src=' + ed[':emo6:'] + '/></a>'
-            +bs+
-            '<a href="::emo7:"><img src=' + ed[':emo7:'] + '/></a>'
-            +bs+
-            '<a href="::emo8:"><img src=' + ed[':emo8:'] + '/></a>'
-            +bs+
-            '<a href="::emo9:"><img src=' + ed[':emo9:'] + '/></a>'
-            +bs+
+            '<a href="::emo6:"><img src=' + ed[':emo6:'] + '/></a>'+bs+
+            '<a href="::emo7:"><img src=' + ed[':emo7:'] + '/></a>'+bs+
+            '<a href="::emo8:"><img src=' + ed[':emo8:'] + '/></a>'+bs+
+            '<a href="::emo9:"><img src=' + ed[':emo9:'] + '/></a>'+bs+
             '<a href="::emo10:"><img src=' + ed[':emo10:'] + '/></a>'
-            '<a href="::emo11:"><img src=' + ed[':emo11:'] + '/></a>'
-            +bs+
-            '<a href="::emo12:"><img src=' + ed[':emo12:'] + '/></a>'
-            +bs+
-            '<a href="::emo13:"><img src=' + ed[':emo13:'] + '/></a>'
-            +bs+
-            '<a href="::emo14:"><img src=' + ed[':emo14:'] + '/></a>'
-            +bs+
+            '<a href="::emo11:"><img src=' + ed[':emo11:'] + '/></a>'+bs+
+            '<a href="::emo12:"><img src=' + ed[':emo12:'] + '/></a>'+bs+
+            '<a href="::emo13:"><img src=' + ed[':emo13:'] + '/></a>'+bs+
+            '<a href="::emo14:"><img src=' + ed[':emo14:'] + '/></a>'+bs+
             '<a href="::emo15:"><img src=' + ed[':emo15:'] + '/></a>'
             )
         
